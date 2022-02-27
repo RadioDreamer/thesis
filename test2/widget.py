c1082808c5e9deffc7eda48120c26212849c1ffd
Levente Buzga <buzgalevi@gmail.com>
2022-02-27 15:42:17 +0100

    added thesis project

--- test2/widget.py
+++ test2/widget.py
@@ -1,15 +1,16 @@
-# This Python file uses the following encoding: utf-8
 import sys
-from PySide6.QtWidgets import QApplication, QWidget
+
+from PySide6.QtWidgets import QApplication, QMainWindow
 
 
-class Widget(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
-        QWidget.__init__(self)
+        super().__init__()
+
+        self.setWindowTitle("Membránrendszer szimuláció")
 
 
-if __name__ == "__main__":
-    app = QApplication([])
-    window = Widget()
-    window.show()
-    sys.exit(app.exec_())
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
