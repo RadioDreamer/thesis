bf3580fc6ac47960eedf5c7a62aef30f6aa551c4
Levente Buzga <buzgalevi@gmail.com>
2022-03-16 18:39:15 +0100

    updated model with base_model implementation

--- thesis/test/UnitTests.py
+++ thesis/test/UnitTests.py
@@ -1,5 +1,6 @@
 import sys
 import pytest
+
 sys.path.append("/home/levi/ELTE/ELTE6/SZAKDOGA/thesis/thesis/model")
 from MultiSet import (
     MultiSet,
@@ -13,6 +14,8 @@ from MembraneStructure import (
     MembraneStructure
 )
 
+from Region import Region
+
 from Rule import (
     Rule,
     Direction,
@@ -23,6 +26,8 @@ from Rule import (
     SymportRule,
     InvalidTypeException
 )
+
+from BaseModel import BaseModel
 
 
 def test_multiset():
@@ -93,7 +98,8 @@ def test_membrane_structure():
     assert ms.result == n
 
     n.children[0].add_child(Node())
-    ms.preorder(ms.skin, ms.get_all_children, lambda x: x.id == 0)
+    ms.preorder(ms.skin, MembraneStructure.get_all_children,
+                lambda x: x.id == 0)
     assert ms.result == n.children
 
     ms.preorder(ms.skin, ms.add_node, lambda x: x.id == 3)
@@ -146,3 +152,92 @@ def test_rule():
     rule9 = SymportRule(TransportationRuleType.SYMPORT_OUT, exported_obj=exp)
     assert rule9.weight() == 2
     assert rule9.imported_obj is None
+
+
+def test_base_model():
+    n = Node()
+    ms = MembraneStructure(n)
+
+    rule_0 = BaseModelRule({'a': 1, 'b': 1}, {('c', Direction.HERE): 2})
+    rule_00 = BaseModelRule({'a': 1}, {('d', Direction.OUT): 2})
+    obj_0 = {'a': 2, 'b': 1}
+    region_0 = Region(n.id, None, objects=obj_0, rules=[rule_0, rule_00])
+
+    regions = {0: region_0}
+    model = BaseModel(tree=ms, regions=regions)
+    assert model.is_dissolving is False
+    assert model.any_rule_applicable() is True
+
+    model.apply(rule_0, region_0)
+    assert region_0.new_objects.multiplicity('c') == 2
+    assert region_0.objects.multiplicity('a') == 1
+    print(region_0.objects)
+    with pytest.raises(ObjectNotFoundException) as _:
+        region_0.objects.multiplicity('b')
+
+    region_0.new_objects = None
+    region_0.objects = MultiSet({'a': 2, 'b': 1})
+
+    print(model.tree.get_root_id())
+    model.apply(rule_00, region_0)
+    assert len(region_0.objects) == 2
+    assert region_0.objects.multiplicity('a') == 1
+    assert model.any_rule_applicable()
+    model.apply(rule_00, region_0)
+    assert len(region_0.objects) == 1
+    with pytest.raises(ObjectNotFoundException) as _:
+        region_0.objects.multiplicity('a')
+    assert model.any_rule_applicable() is False
+
+    rule_000 = BaseModelRule({'a': 1}, {('f', Direction.IN): 2})
+    region_0.rules.append(rule_000)
+    assert model.is_applicable(rule_000, region_0) is False
+    assert model.any_rule_applicable() is False
+
+    n1 = Node()
+    n1.add_child(Node())
+    n1.children[0].add_child(Node())
+    ms1 = MembraneStructure(n1)
+
+    rule_a = BaseModelRule({'a': 1},
+                           {('b', Direction.HERE): 1, ('a', Direction.HERE): 1})
+    rule_b = DissolvingRule({'a': 1},
+                            {('b', Direction.HERE): 1})
+    rule_c = BaseModelRule({'c': 1},
+                           {('c', Direction.HERE): 2})
+    inner_rules = [rule_a, rule_b, rule_c]
+    inner_objects = {'a': 1, 'c': 1}
+
+    strong_rule = BaseModelRule({'c': 2}, {('c', Direction.HERE): 1})
+    weak_rule = DissolvingRule({'c': 2}, {})
+    rule_d = PriorityRule(strong_rule, weak_rule)
+
+    rule_e = BaseModelRule({'b': 1}, {('d', Direction.HERE): 1})
+    rule_f = BaseModelRule({'d': 1},
+                           {('d', Direction.HERE): 1, ('e', Direction.HERE): 1})
+
+    middle_rules = [rule_d, rule_e, rule_f]
+
+    outer_id = n1.id
+    middle_id = n1.children[0].id
+    inner_id = n1.children[0].children[0].id
+
+    region_inner = Region(inner_id, middle_id,
+                          objects=inner_objects, rules=inner_rules
+                          )
+    region_middle = Region(middle_id, outer_id, None, middle_rules)
+
+    outer_rule = [BaseModelRule({'e': 1}, {('e', Direction.OUT): 1})]
+    region_outer = Region(outer_id, None, None, outer_rule)
+    regions = {outer_id: region_outer, middle_id: region_middle,
+               inner_id: region_inner}
+
+    model = BaseModel(tree=ms1, regions=regions)
+    print("SIMULATE")
+    print(model.regions)    
+    model.simulate_step()
+    print(model.regions)
+
+    model.simulate_step()
+    print(model.regions)
+test_base_model()
