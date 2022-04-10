40cc833e8a3919737feb7799922e3d451cbb7f46
Levente Buzga <buzgalevi@gmail.com>
2022-04-10 12:15:51 +0200

    working nested region generation

--- thesis/view/MembraneSimulator.py
+++ thesis/view/MembraneSimulator.py
@@ -27,7 +27,7 @@ class MembraneSimulator(QWidget):
         self.model = None
         self.scene = QGraphicsScene()
         self.view = QGraphicsView()
-        self.regions = {}
+        self.view_regions = {}
         self.view.setScene(self.scene)
 
         self.max_width = max_width
@@ -52,29 +52,64 @@ class MembraneSimulator(QWidget):
         self.model.signal.sim_over.connect(self.simulation_over)
         self.model.signal.obj_changed.connect(self.update_obj_view)
         self.model.signal.rules_changed.connect(self.update_rule_view)
+        self.model.signal.region_dissolved.connect(self.update_dissolve)
         self.draw_model()
 
+    def update_dissolve(self, id):
+        self.scene.removeItem(self.view_regions[id])
+        del self.view_regions[id]
+
     def update_obj_view(self, id, string):
-        self.regions[id].obj_text.setText(string)
+        self.view_regions[id].obj_text.setText(string)
 
     def update_rule_view(self, id, string):
-        print("UPDATE", string)
-        self.regions[id].rule_text.setText(string)
+        self.view_regions[id].rule_text.setText(string)
 
     def draw_model(self):
         self.scene.clear()
         self.skin_id = self.model.get_root_id()
-        self.regions[self.skin_id] = RegionView(self.skin_id,
-                                                QRectF(0, 0, self.max_width,
-                                                       self.max_height), str(
-                self.model.regions[self.skin_id].objects), "", simulator=self)
-        self.scene.addItem(self.regions[self.skin_id])
+        self.view_regions[self.skin_id] = RegionView(self.skin_id,
+                                                     QRectF(0, 0,
+                                                            self.max_width,
+                                                            self.max_height),
+                                                     str(
+                                                         self.model.regions[
+                                                             self.skin_id].objects),
+                                                     "", simulator=self)
+        self.scene.addItem(self.view_regions[self.skin_id])
 
-        children = self.model.get_child(self.model.regions[self.skin_id])
-        self.regions[children.id] = RegionView(children.id,
-                                               QRectF(0, 0, self.max_width / 2,
-                                                      self.max_height / 2), str(
-               self.model.regions[children.id].objects), "", simulator=self, parent=self.regions[self.skin_id])
+        gen_child_list = [self.skin_id]
+        while gen_child_list:
+            current_node_id = gen_child_list[0]
+            if self.model.get_num_of_children(
+                    self.model.regions[current_node_id]) == 0:
+                gen_child_list.remove(current_node_id)
+            else:
+                children = self.model.get_all_children(
+                    self.model.regions[current_node_id])
+                child_width = self.view_regions[
+                                  current_node_id].rect().width() / (2 * len(
+                    children))
+                child_height = self.view_regions[
+                                   current_node_id].rect().height() / (2 * len(
+                    children))
+
+                for i, child in enumerate(children):
+                    gen_child_list.append(child.id)
+                    self.view_regions[child.id] = RegionView(child.id,
+                                                             QRectF(0, 0,
+                                                                    child_width,
+                                                                    child_height),
+                                                             str(
+                                                                 self.model.regions[
+                                                                     child.id].objects),
+                                                             "",
+                                                             simulator=self,
+                                                             parent=
+                                                             self.view_regions[
+                                                                 current_node_id])
+                    self.view_regions[child.id].setPos(i * child_width, 0)
+                gen_child_list.remove(current_node_id)
         self.view.show()
 
     @classmethod
@@ -82,13 +117,13 @@ class MembraneSimulator(QWidget):
         return MembraneSystem.is_valid_parentheses(string)
 
     def show_membranes(self):
-        self.regions[self.skin_id].show()
-
+        self.view_regions[self.skin_id].show()
+    
     def hide_membranes(self):
-        self.regions[self.skin_id].hide()
+        self.view_regions[self.skin_id].hide()
 
     def is_visible(self):
-        return self.regions[self.skin_id].isVisible()
+        return self.view_regions[self.skin_id].isVisible()
 
     def update_region_objects(self, id, new_objects: str):
         new_multiset = MultiSet.string_to_multiset(new_objects)
@@ -102,5 +137,4 @@ class MembraneSimulator(QWidget):
         self.model.simulate_step()
 
     def simulate_computation(self):
-        print(self.model)
         self.model.simulate_computation()
--- thesis/view/RegionView.py
+++ thesis/view/RegionView.py
@@ -7,6 +7,7 @@ from PySide6.QtCore import QRectF
 from PySide6.QtGui import QPen, QBrush, QColor
 from RuleAndObjectEdtDialog import RuleAndObjectEditDialog
 
+
 class RegionView(QGraphicsRectItem):
     def __init__(self, id, rect, obj, rules, simulator=None, parent=None):
         super().__init__(rect, parent)
@@ -29,89 +30,89 @@ class RegionView(QGraphicsRectItem):
         self.click_rect = None
 
     ##################################
-#    def mousePressEvent(self, event):
-#        self.click_pos = event.pos()
-#        rect = self.rect()
-#        if abs(rect.left() - self.click_pos.x()) < 5:
-#            self.selected_edge = 'left'
-#        elif abs(rect.right() - self.click_pos.x()) < 5:
-#            self.selected_edge = 'right'
-#        elif abs(rect.top() - self.click_pos.y()) < 5:
-#            self.selected_edge = 'top'
-#        elif abs(rect.bottom() - self.click_pos.y()) < 5:
-#            self.selected_edge = 'bottom'
-#        else:
-#            self.selected_edge = None
-#        self.click_pos = event.pos()
-#        self.click_rect = rect
-#        super().mousePressEvent(event)
+    #    def mousePressEvent(self, event):
+    #        self.click_pos = event.pos()
+    #        rect = self.rect()
+    #        if abs(rect.left() - self.click_pos.x()) < 5:
+    #            self.selected_edge = 'left'
+    #        elif abs(rect.right() - self.click_pos.x()) < 5:
+    #            self.selected_edge = 'right'
+    #        elif abs(rect.top() - self.click_pos.y()) < 5:
+    #            self.selected_edge = 'top'
+    #        elif abs(rect.bottom() - self.click_pos.y()) < 5:
+    #            self.selected_edge = 'bottom'
+    #        else:
+    #            self.selected_edge = None
+    #        self.click_pos = event.pos()
+    #        self.click_rect = rect
+    #        super().mousePressEvent(event)
 
-#    def mouseMoveEvent(self, event):
-#        pos = event.pos()
-#        x_diff = pos.x() - self.click_pos.x()
-#        y_diff = pos.y() - self.click_pos.y()
+    #    def mouseMoveEvent(self, event):
+    #        pos = event.pos()
+    #        x_diff = pos.x() - self.click_pos.x()
+    #        y_diff = pos.y() - self.click_pos.y()
 
-#        # Start with the rectangle as it was when clicked.
-#        rect = QRectF(self.click_rect)
+    #        # Start with the rectangle as it was when clicked.
+    #        rect = QRectF(self.click_rect)
 
-#        # Then adjust by the distance the mouse moved.
-#        if self.selected_edge is None:
-#            rect.translate(x_diff, y_diff)
-#        elif self.selected_edge == 'top':
-#            rect.adjust(0, y_diff, 0, 0)
-#        elif self.selected_edge == 'left':
-#            rect.adjust(x_diff, 0, 0, 0)
-#        elif self.selected_edge == 'bottom':
-#            rect.adjust(0, 0, 0, y_diff)
-#        elif self.selected_edge == 'right':
-#            rect.adjust(0, 0, x_diff, 0)
+    #        # Then adjust by the distance the mouse moved.
+    #        if self.selected_edge is None:
+    #            rect.translate(x_diff, y_diff)
+    #        elif self.selected_edge == 'top':
+    #            rect.adjust(0, y_diff, 0, 0)
+    #        elif self.selected_edge == 'left':
+    #            rect.adjust(x_diff, 0, 0, 0)
+    #        elif self.selected_edge == 'bottom':
+    #            rect.adjust(0, 0, 0, y_diff)
+    #        elif self.selected_edge == 'right':
+    #            rect.adjust(0, 0, x_diff, 0)
 
-#        # Figure out the limits of movement. I did it by updating the scene's
-#        # rect after the window resizes.
-#        scene_rect = self.scene().sceneRect()
-#        view_left = scene_rect.left()
-#        view_top = scene_rect.top()
-#        view_right = scene_rect.right()
-#        view_bottom = scene_rect.bottom()
+    #        # Figure out the limits of movement. I did it by updating the scene's
+    #        # rect after the window resizes.
+    #        scene_rect = self.scene().sceneRect()
+    #        view_left = scene_rect.left()
+    #        view_top = scene_rect.top()
+    #        view_right = scene_rect.right()
+    #        view_bottom = scene_rect.bottom()
 
-#        # Next, check if the rectangle has been dragged out of bounds.
-#        if rect.top() < view_top:
-#            if self.selected_edge is None:
-#                rect.translate(0, view_top - rect.top())
-#            else:
-#                rect.setTop(view_top)
-#        if rect.left() < view_left:
-#            if self.selected_edge is None:
-#                rect.translate(view_left - rect.left(), 0)
-#            else:
-#                rect.setLeft(view_left)
-#        if view_bottom < rect.bottom():
-#            if self.selected_edge is None:
-#                rect.translate(0, view_bottom - rect.bottom())
-#            else:
-#                rect.setBottom(view_bottom)
-#        if view_right < rect.right():
-#            if self.selected_edge is None:
-#                rect.translate(view_right - rect.right(), 0)
-#            else:
-#                rect.setRight(view_right)
+    #        # Next, check if the rectangle has been dragged out of bounds.
+    #        if rect.top() < view_top:
+    #            if self.selected_edge is None:
+    #                rect.translate(0, view_top - rect.top())
+    #            else:
+    #                rect.setTop(view_top)
+    #        if rect.left() < view_left:
+    #            if self.selected_edge is None:
+    #                rect.translate(view_left - rect.left(), 0)
+    #            else:
+    #                rect.setLeft(view_left)
+    #        if view_bottom < rect.bottom():
+    #            if self.selected_edge is None:
+    #                rect.translate(0, view_bottom - rect.bottom())
+    #            else:
+    #                rect.setBottom(view_bottom)
+    #        if view_right < rect.right():
+    #            if self.selected_edge is None:
+    #                rect.translate(view_right - rect.right(), 0)
+    #            else:
+    #                rect.setRight(view_right)
 
-#        # Also check if the rectangle has been dragged inside out.
-#        if rect.width() < 5:
-#            if self.selected_edge == 'left':
-#                rect.setLeft(rect.right() - 5)
-#            else:
-#                rect.setRight(rect.left() + 5)
-#        if rect.height() < 5:
-#            if self.selected_edge == 'top':
-#                rect.setTop(rect.bottom() - 5)
-#            else:
-#                rect.setBottom(rect.top() + 5)
+    #        # Also check if the rectangle has been dragged inside out.
+    #        if rect.width() < 5:
+    #            if self.selected_edge == 'left':
+    #                rect.setLeft(rect.right() - 5)
+    #            else:
+    #                rect.setRight(rect.left() + 5)
+    #        if rect.height() < 5:
+    #            if self.selected_edge == 'top':
+    #                rect.setTop(rect.bottom() - 5)
+    #            else:
+    #                rect.setBottom(rect.top() + 5)
 
-#        # Finally, update the rect that is now guaranteed to stay in bounds.
-#        self.setRect(rect)
-#        self.center_text()
-#        super().mouseMoveEvent(event)
+    #        # Finally, update the rect that is now guaranteed to stay in bounds.
+    #        self.setRect(rect)
+    #        self.center_text()
+    #        super().mouseMoveEvent(event)
 
     ##################################
     def center_text(self):
@@ -128,20 +129,18 @@ class RegionView(QGraphicsRectItem):
 
         valid_fn = self.simulator.model.__class__.is_valid_rule
         dialog = RuleAndObjectEditDialog(str(objects), rule_string,
-                                                 self.simulator.type,
-                                                 valid_fn=valid_fn
-                                                 )
+                                         self.simulator.type,
+                                         valid_fn=valid_fn
+                                         )
         dialog.exec()
 
         rule_result = dialog.get_rules()
-        print("RESULT", rule_result)
         if not rule_result == rule_string:
             self.simulator.update_region_rules(self.id, rule_result)
 
         obj_result = RegionView.sort_word(dialog.object_edit.text())
         if obj_result != RegionView.sort_word(self.obj_text.text()):
             self.simulator.update_region_objects(self.id, obj_result)
-
         self.center_text()
 
     @classmethod
